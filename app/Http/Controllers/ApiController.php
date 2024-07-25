5c44b817b8dfcf6e64126e0003990689e2f26047
panggilajafangki2002 <panggilajafangki2002@gmail.com>
2024-07-25 07:55:08 +0000

    fixed: user api validator lbrary not imported

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
 
 
 class ApiController extends Controller
