a7dabe30931281c5847413fcf60344d05766ee42
panggilajafangki2002 <panggilajafangki2002@gmail.com>
2024-07-25 08:15:45 +0000

    fixed: add user api resources

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\UserResource;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
