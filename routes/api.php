14f311b566a53d979de0ff46fcce80695c2c9304
panggilajafangki2002 <panggilajafangki2002@gmail.com>
2024-07-29 00:25:36 +0000

    feat: add jwt auth

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,15 @@ use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\AuthApiController;
 use App\Http\Controllers\Api\ProductTypeController;
 
 Route::resource('users',ApiController::class);
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('login', [AuthApiController::class, 'login']);
+    Route::post('logout', [AuthApiController::class,'logout']);
+    Route::post('refresh', [AuthApiController::class,'refresh']);
+    Route::post('me', [AuthApiController::class,'me']);
+});
 Route::resource('products',ProductController::class);
 Route::resource('types',ProductTypeController::class);
-
